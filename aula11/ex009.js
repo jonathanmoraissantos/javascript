8df700390e7097bb49301bed7aba7ffe7a826e2f
Jonathan Morais <jonathanjsantos@icloud.com>
2022-06-04 10:57:18 -0300

    inicio com arquivos .js

---
+++ aula11/ex009.js
@@ -0,0 +1,9 @@
+var pais = 'Brasil'
+
+console.log(`Vivendo em ${pais}`)
+
+if(pais=='Brasil') { //condição composta
+    console.log('Você é Brasileiro!')
+} else{
+    console.log('Você é Estrangeiro!')
+}
