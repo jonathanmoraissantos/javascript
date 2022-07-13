9522edf2e3596ddc37087b008ed36e2ff576f383
Jonathan Santos <jonathanjsantos@icloud.com>
2022-07-13 20:08:52 -0300

    Creating ex17

---
+++ aula14ex/ex017/script.js
@@ -0,0 +1,18 @@
+function calcular(){
+    var f_numero = document.getElementById('f_numero').value
+    var s_resultado = document.getElementById('s_resultado').value
+
+    var n_numero = Number(f_numero)
+
+    if(f_numero == "" || f_numero == NaN){
+
+        window.alert(`Selecione um número válido.`)
+
+    } else {
+        for(var c=f_numero;c<=0;c--){
+            
+            s_resultado.
+        }
+
+    }
+}
