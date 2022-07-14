3aa915acf5b5a38233b39d9a134ffb2104f4142f
Jonathan Santos <jonathanjsantos@icloud.com>
2022-07-14 20:34:07 -0300

    final

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -4,29 +4,33 @@ function calcular() {
 
     var n_numero = Number(f_numero)
 
-
+    //Testa se número não é null
     if (f_numero == "" || f_numero == NaN) {
 
         window.alert(`Selecione um número válido.`)
 
     } else {
-
+        //cria variável que vai receber o texto 'número x número = resultado' a cada for
         var txt = ""
-        s_resultado.length = 0;
+        s_resultado.length = 0
 
+        //c é igual à zero. enquanto c for menor ou igual à dez = texto recebe string
+        //em seguida, select 's_resultado' adiciona um novo 'option' anônimo (mesmo que var option = new Option(txt, txt)). 
         for (var c = 0; c <= 10; c++) {
             txt = `${n_numero} x ${c} = ${n_numero * c}`
-            var option = new Option(txt, txt)
-            s_resultado.add(option)
+            s_resultado.add(new Option(txt, txt))
         }
 
     }
 }
 
-function clear(){
+function limpar() {
     var f_numero = document.getElementById('f_numero')
     var s_resultado = document.getElementById('s_resultado')
 
+    //limpar todos os campos e restaurar ao original
     s_resultado.length = 0
+    s_resultado.add(new Option('Digite um número', 'dgt'))
+    f_numero.value = ""
 
 }
