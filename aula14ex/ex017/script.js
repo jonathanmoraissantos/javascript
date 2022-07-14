cc746412d2485aa264b49d8819ed993167c1a2bf
Jonathan Santos <jonathanjsantos@icloud.com>
2022-07-14 20:08:41 -0300

    update

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,18 +1,32 @@
-function calcular(){
+function calcular() {
     var f_numero = document.getElementById('f_numero').value
-    var s_resultado = document.getElementById('s_resultado').value
+    var s_resultado = document.getElementById('s_resultado')
 
     var n_numero = Number(f_numero)
 
-    if(f_numero == "" || f_numero == NaN){
+
+    if (f_numero == "" || f_numero == NaN) {
 
         window.alert(`Selecione um número válido.`)
 
     } else {
-        for(var c=f_numero;c<=0;c--){
-            
-            s_resultado.
+
+        var txt = ""
+        s_resultado.length = 0;
+
+        for (var c = 0; c <= 10; c++) {
+            txt = `${n_numero} x ${c} = ${n_numero * c}`
+            var option = new Option(txt, txt)
+            s_resultado.add(option)
         }
 
     }
+}
+
+function clear(){
+    var f_numero = document.getElementById('f_numero')
+    var s_resultado = document.getElementById('s_resultado')
+
+    s_resultado.length = 0
+
 }
