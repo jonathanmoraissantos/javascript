7e986f97ce6ab9ddb1ca6f4e454bc50220361ec3
Jonathan Santos <jonathanjsantos@icloud.com>
2022-07-15 20:01:54 -0300

    ajustes propostos por professor

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,24 +1,24 @@
 function contar() {
     //Coleta valores digitados nos formulários
-    var f_inicio = document.getElementById('f_inicio').value
-    var f_fim = document.getElementById('f_fim').value
-    var f_passo = document.getElementById('f_passo').value
+    var f_inicio = document.getElementById('f_inicio')
+    var f_fim = document.getElementById('f_fim')
+    var f_passo = document.getElementById('f_passo')
 
     //Converte valores do formulário em tipo Number
-    var n_inicio = Number(f_inicio)
-    var n_fim = Number(f_fim)
-    var n_passo = Number(f_passo)
+    var n_inicio = Number(f_inicio.value)
+    var n_fim = Number(f_fim.value)
+    var n_passo = Number(f_passo.value)
 
     //Onde resultado será armazenado e exibido
     var res = document.getElementById('res')
-    var txt = ''
+    var txt = 'Contando: <br>'
 
     //Tratamento dos valores inseridos no formulário
     if (n_inicio > n_fim) {
 
         window.alert(`Operação inválida! O número de início não pode ser menor que o fim.`)
 
-    } else if (n_passo > (n_fim - n_inicio)) {
+    } else if (n_passo > (n_fim - n_inicio) || n_passo<=0) {
 
         window.alert(`Passo inválido! Selecione um valor menor ou igual a ${n_fim - n_inicio}.`)
 
