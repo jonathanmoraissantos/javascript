8da7ce31cce4d172765922271baf18ada2343421
Jonathan Santos <jonathanjsantos@icloud.com>
2022-07-23 09:25:42 -0300

    Update ambiente.js

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,3 +1,23 @@
 var num = [5, 8, 2, 9, 3]
+num.push(1)
 
 console.log(`Nosso vetor tem os seguintes valors ${num}`)
+
+console.log(`Ordenando array ${num.sort()}`)
+
+for (let c = 0; c < num.length; c++) {
+    console.log(`Posição: ${c} | Valor: ${num[c]}`)
+}
+
+//for com in
+for (let c in num) {
+    console.log(num[c])
+}
+
+//método indexOf()
+let pos = num.indexOf(4)
+if (pos == -1) {
+    console.log("O valor não foi encontrado.")
+} else {
+    console.log(`O valor está na posição ${pos}`)
+}
